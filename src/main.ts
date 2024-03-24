ec4281beaf0212dec3450b29e3ced673233c060d
nhatminh6112003 <106549349+nhatminh6112003@users.noreply.github.com>
2024-03-24 13:04:29 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document, {
+  SwaggerModule.setup('api', app, document, {
     customSiteTitle: 'Backend Generator',
     customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [

8aa624882a3bf80fe9b82619745f829d1fb54d56
nhatminh6112003 <106549349+nhatminh6112003@users.noreply.github.com>
2024-03-24 12:59:39 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,27 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Backend Generator')
+    .setDescription('Documentation API Test')
     .setVersion('1.0')
-    .addTag('cats')
+    .setBasePath('api/v1')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document, {
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
   app.enableCors();
 
   app.useGlobalPipes(
